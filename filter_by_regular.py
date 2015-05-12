1840635425f8561afcc72cae25a54924e926deec
Arka Roy <arka1001@gmail.com>
2015-05-12 09:24:38 -0400

    filter by a column name

--- filter_by_regular.py
+++ filter_by_regular.py
@@ -21,3 +21,4 @@ def filter_by_regular(filename):
     turnstile_data = pandas.read_csv(filename)
     turnstile_data = turnstile_data[turnstile_data['DESCn']=='REGULAR']
     return turnstile_data
+
