0e65c3a9d787e3483b2a2c36fedb15634c0dbeb6
Arka Roy <arka1001@gmail.com>
2015-05-11 20:02:55 -0400

    Create api_get_request.py

---
+++ api_get_request.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+def api_get_request(url):
+    # In this exercise, you want to call the last.fm API to get a list of the
+    # top artists in Spain.
+    #
+    # Once you've done this, return the name of the number 1 top artist in Spain.
+    
+    response = requests.get(url)
+    top_artists = json.loads(response.text)
+    return top_artists['topartists']['artist'][0]['name']
